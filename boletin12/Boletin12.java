46075fac583e8040fb6fa8b08411a3de67c384d5
hfaildeestevez <72596856+hfaildeestevez@users.noreply.github.com>
2021-01-27 21:02:37 +0100

    Add files via upload

---
+++ boletin12/Boletin12.java
@@ -0,0 +1,29 @@
+
+package boletin12;
+
+import javax.swing.JOptionPane;
+
+public class Boletin12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+             Garaje g=new Garaje();
+        int num=Integer.parseInt(JOptionPane.showInputDialog("Introduce numero de coches aparcados"));
+        g.setNumeroCoche(num);
+        if(num>=0 && num<5){
+           System.out.println("PLAZAS DISPONIBLES");
+           g.setCoche(JOptionPane.showInputDialog("Introduce matricula"));
+           g.setHoras(Float.parseFloat(JOptionPane.showInputDialog("Introduce tiempo de estancia")));
+           g.precio2();
+           g.recivido(Float.parseFloat(JOptionPane.showInputDialog("Introduce dinero")));
+           g.factura();
+       }else if(num==5){
+           System.out.println("COMPLETO");
+
+       }
+
+    }
+
+}
